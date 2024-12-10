273bcbc6b93629c44f4e54254565e8b449f98eb7
matas234 <160629386+matas234@users.noreply.github.com>
2024-12-10 08:45:33 +0200

    day10

--- day10.py
+++ day10.py
@@ -5,6 +5,10 @@ with open('input10.txt', 'r') as f:
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 
+def in_bounds(x, y):
+    return 0 <= x < len(data) and 0 <= y < len(data[0])
+
+
 def dfs_part1(x, y, locs_of_nines, depth = 1):
     if depth == 10 and (x, y) not in locs_of_nines:
         locs_of_nines.add((x, y))
@@ -12,26 +16,22 @@ def dfs_part1(x, y, locs_of_nines, depth = 1):
 
 
     return sum(
-        dfs_part1(x + dx, y + dy, locs_of_nines, depth + 1)
+        dfs_part1(nx, ny, locs_of_nines, depth + 1)
         for dx, dy in dirs
-        if 0 <= x + dx < len(data)
-        and 0 <= y + dy < len(data[0])
-        and data[x + dx][y + dy] == str(depth)
+        if in_bounds(nx := x + dx,
+                     ny :=  y + dy
+                     )
+        and data[nx][ny] == str(depth)
         )
 
 
 
-
 def part1():
-    sum = 0
+    return sum(dfs_part1(x, y, set())
+               for x in range(len(data))
+               for y in range(len(data[0]))
+               if data[x][y] == '0')
 
-    for x in range(len(data)):
-        for y in range(len(data[0])):
-
-            if data[x][y] == '0':
-                sum += dfs_part1(x, y, set())
-
-    return sum
 
 print(f"Part 1: {part1()}")
 
@@ -41,25 +41,21 @@ def dfs_part2(x, y, depth = 1):
     if depth == 10:
         return 1
 
-
     return sum(
-        dfs_part2(x + dx, y + dy, depth + 1)
+        dfs_part2(nx, ny, depth + 1)
         for dx, dy in dirs
-        if 0 <= x + dx < len(data)
-        and 0 <= y + dy < len(data[0])
-        and data[x + dx][y + dy] == str(depth)
+        if in_bounds(nx := x + dx,
+                     ny :=  y + dy
+                     )
+        and data[nx][ny] == str(depth)
         )
 
 
 def part2():
-    sum = 0
-
-    for x in range(len(data)):
-        for y in range(len(data[0])):
-
-            if data[x][y] == '0':
-                sum += dfs_part2(x, y)
-
-    return sum
+    return sum(dfs_part2(x, y)
+               for x in range(len(data))
+               for y in range(len(data[0]))
+               if data[x][y] == '0'
+               )
 
 print(f"Part 2: {part2()}")
